bd68c139b1236ac1b1ee7eb696dab2b4fe8af6c3
Irfan TOOR <email@irfantoor.com>
2021-02-25 03:20:29 +0100

    removed not-used object-reflector from composer.json

--- src/Test.php
+++ src/Test.php
@@ -39,7 +39,7 @@ class Test
 {
     const NAME        = "Unit Test";
     const DESCRIPTION = "and I test ....";
-    const VERSION     = "0.7.7";
+    const VERSION     = "0.7.8";
 
     /**
      * Constants describing different assertion statuses
